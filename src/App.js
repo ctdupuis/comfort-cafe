b645b7aec90a03500d435a2231e9aabb0656b022
Cody Dupuis <codydupuis@Codys-iMac.lusfiber.net>
2021-11-02 15:47:36 -0500

    react-router-dom installed, BroswerRouter component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
 import Header from './components/static/Header';
 import Nav from './components/static/Nav';
+import Home from './components/static/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <div className="wrapper">
+        <Home />
+      </div>
     </div>
   );
 }
