6222d86097e74e73b5eae17946517fb7f2d2546d
Cody Dupuis <codydupuis@Codys-iMac.lusfiber.net>
2021-11-02 16:18:49 -0500

    nav links established

--- src/components/static/Nav.jsx
+++ src/components/static/Nav.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import '../../stylesheets/nav.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
     return (
         <nav>
             <div class="nav-container">
-                <div class="nav-item">Menu</div>
-                <div class="nav-item">Locations</div>
-                <div class="nav-item">Order</div>
-                <div class="nav-item">About us</div>
-                <div class="nav-item">Careers</div>
+                <div class="nav-item">
+                    <NavLink to={"/"}>
+                        Home
+                    </NavLink>
+                </div>
+
+                <div class="nav-item">
+                    <NavLink to={"/menu"}>
+                        Menu
+                    </NavLink>
+                </div>
+
+                <div class="nav-item">
+                    <NavLink to={"/locations"}>
+                        Locations
+                    </NavLink>
+                </div>
+
+                <div class="nav-item">
+                    <NavLink to={"/order"}>
+                        Order
+                    </NavLink>
+                </div>
+
+                <div class="nav-item">
+                    <NavLink to={"/about"}>
+                        About us 
+                    </NavLink>
+                </div>
+
+                <div class="nav-item">
+                    <NavLink to={"/careers"}>
+                        Careers
+                    </NavLink>
+                </div>
             </div>
         </nav>
     )
