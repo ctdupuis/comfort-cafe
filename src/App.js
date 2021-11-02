f05d95dd6409ae80afbac87b31238c190c5fad8a
Cody Dupuis <codydupuis@Codys-iMac.lusfiber.net>
2021-11-02 15:26:32 -0500

    added Nav component/stylesheeet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/static/Header';
+import Nav from './components/static/Nav';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/static/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../stylesheets/nav.css';
+
+export default function Nav() {
+    return (
+        <nav>
+            <div class="nav-container">
+                <div class="nav-item">Menu</div>
+                <div class="nav-item">Locations</div>
+                <div class="nav-item">Order</div>
+                <div class="nav-item">About us</div>
+                <div class="nav-item">Careers</div>
+            </div>
+        </nav>
+    )
+};
