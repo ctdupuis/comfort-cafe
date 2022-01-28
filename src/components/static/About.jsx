44eadff0a663cb9dff4b15b1b44c800518f45eed
Cody <relationalprogrammer@gmail.com>
2022-01-28 12:31:49 -0600

    About.jsx styled for mobile with placeholder text

--- src/components/static/About.jsx
+++ src/components/static/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "../../stylesheets/about.css"
 
 export default function About() {
     return (
-        <div>
-            This is the About Page
+        <div className="wrapper">
+            <div className="about-bg">
+                <div className="about-title">
+                    <h2>Welcome to the Comfort Café</h2>
+                </div>
+                <div className="about-title">
+                    <h3>Who We Are</h3>
+                    <div className="about-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className="about-title">
+                    <h3>What We're About</h3>
+                    <div className="about-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
